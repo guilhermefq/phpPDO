8813c30cc9a3be4234529deda6eeb925e806cd5e
guilhermefq <gfantuci@gmail.com>
2018-08-18 00:47:10 -0400

    Fim curso Parte 1

--- classes/Categoria.php
+++ classes/Categoria.php
@@ -16,7 +16,7 @@ class Categoria
 
     public function listar()
     {
-        $query = "SELECT id, nome FROM categorias";
+        $query = "SELECT sid, nome FROM categorias";
         $conexao = Conexao::pegarConexao();
         $resultado = $conexao->query($query);
         $lista = $resultado->fetchAll();
